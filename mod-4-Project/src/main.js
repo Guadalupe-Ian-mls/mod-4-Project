b8fe32c0c5b391d86b3263f6f4f584f7aefb7165
Ian <iansm643@gmail.com>
2026-02-18 12:35:25 -0500

    Totally didnt push to main

--- mod-4-Project/src/main.js
+++ mod-4-Project/src/main.js
@@ -4,18 +4,18 @@ import { renderAnimeDetails, renderAnime } from './dom-helper-functions'
 const errorMessage = document.querySelector('#error-message');
 const animeList = document.querySelector('#anime-list');
 
-animeList.addEventListener('click',async(event)=>{
-    const li = event.target.closest('li');
-    if(!li) return;
-     const anime = await getAnimeByID(li.dataset.productId);
+animeList.addEventListener('click', async (event) => {
+  const li = event.target.closest('li');
+  if (!li) return;
+  const anime = await getAnimeByID(li.dataset.animeId);
 
-    if (!anime) {
-        errorMessage.textContent = 'Failed to load anime details.';
-        setTimeout(() => errorMessage.textContent = '', 2000);
-        return;
-    }
+  if (!anime) {
+    errorMessage.textContent = 'Failed to load anime details.';
+    setTimeout(() => errorMessage.textContent = '', 2000);
+    return;
+  }
 
-    renderAnimeDetails(anime);
+  renderAnimeDetails(anime);
 })
 
 const anime = await getAnime();
